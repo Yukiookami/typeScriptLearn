0cad19955b07dff882901d8dc3056073a76b5389
Yukiookami <37170227+Yukiookami@users.noreply.github.com>
2022-04-05 20:05:50 +0900

    init

---
+++ hello.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: zxy
+ * @Date: 2022-04-05 20:02:58
+ * @LastEditTime: 2022-04-05 20:04:23
+ * @FilePath: /learnTypeScript/hello.ts
+ */
+const hello = (name:string) => {
+  return `hello ${name}`
+}
+
+hello('zxy')
