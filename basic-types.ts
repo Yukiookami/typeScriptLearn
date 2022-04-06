293d335964e7c5d9ca379d8d65b72d3baae65a31
Yukiookami <37170227+Yukiookami@users.noreply.github.com>
2022-04-06 11:17:07 +0900

    2-4 ts基础类型

---
+++ basic-types.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: zxy
+ * @Date: 2022-04-06 11:13:18
+ * @LastEditTime: 2022-04-06 11:15:34
+ * @FilePath: /learnTypeScript/basic-types.ts
+ */
+let isDone: boolean = false
+
+let age: number = 23
+let binaryNumber: number = 0b1111
+
+let firstName: string = 'zxy'
+let message: string = `Hello ${firstName}, age is ${age}`
+
+// undefined和null是所有类型的子类型
+let u: undefined = undefined
+let n: null = null
