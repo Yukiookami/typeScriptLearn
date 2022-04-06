b8b6803e63c5c25c3d3855bf6731c9db323291ac
Yukiookami <37170227+Yukiookami@users.noreply.github.com>
2022-04-06 11:28:03 +0900

    数组和原组

--- basic-types.ts
+++ basic-types.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zxy
  * @Date: 2022-04-06 11:13:18
- * @LastEditTime: 2022-04-06 11:19:53
+ * @LastEditTime: 2022-04-06 11:27:14
  * @FilePath: /learnTypeScript/basic-types.ts
  */
 let isDone: boolean = false
@@ -27,3 +27,19 @@ notSure.getName()
 
 // unitypes 联合类型
 let numberOrString: number | string = 123 || "123"
+
+// 数组
+let arrOfNumbers: number[] = [1, 2, 3, 4]
+arrOfNumbers.push(5)
+// arrOfNumbers.push('23')
+
+// js中的类数组，比如arguments，可以当作数组使用，但是不能使用array的原型方法
+// 在ts中，也给他们对应了相应的类型
+function test () {
+  console.log(arguments)
+  let length = arguments.length
+  // arguments.push(123) error
+}
+
+// 元组 定义多少写多少
+let user: [string, number] = ['sky', 123]
